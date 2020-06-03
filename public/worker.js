84e2e31622c4641b9f9e59986e78ceccdb09d917
Geetesh Gupta <gupta.15@iitj.ac.in>
2020-06-03 12:49:38 +0530

    Update urlsToCache in worker.js

--- public/worker.js
+++ public/worker.js
@@ -1,43 +1,43 @@
-var CACHE_NAME = 'pwa-task-manager';
+var CACHE_NAME = "pwa-task-manager";
 var urlsToCache = [
-  '/',
-  '/static',
+  "/index.html",
+  "/manifest.json",
+  "/worker.js",
+  "/maskable_icon.png",
+  "/static/css",
 ];
 
 // Install a service worker
-self.addEventListener('install', event => {
+self.addEventListener("install", (event) => {
   // Perform install steps
   event.waitUntil(
-    caches.open(CACHE_NAME)
-      .then(function(cache) {
-        console.log('Opened cache');
-        return cache.addAll(urlsToCache);
-      })
+    caches.open(CACHE_NAME).then(function (cache) {
+      console.log("Opened cache");
+      return cache.addAll(urlsToCache);
+    })
   );
 });
 
 // Cache and return requests
-self.addEventListener('fetch', event => {
+self.addEventListener("fetch", (event) => {
   event.respondWith(
-    caches.match(event.request)
-      .then(function(response) {
-        // Cache hit - return response
-        if (response) {
-          return response;
-        }
-        return fetch(event.request);
+    caches.match(event.request).then(function (response) {
+      // Cache hit - return response
+      if (response) {
+        return response;
       }
-    )
+      return fetch(event.request);
+    })
   );
 });
 
 // Update a service worker
-self.addEventListener('activate', event => {
-  var cacheWhitelist = ['pwa-task-manager'];
+self.addEventListener("activate", (event) => {
+  var cacheWhitelist = ["pwa-task-manager"];
   event.waitUntil(
-    caches.keys().then(cacheNames => {
+    caches.keys().then((cacheNames) => {
       return Promise.all(
-        cacheNames.map(cacheName => {
+        cacheNames.map((cacheName) => {
           if (cacheWhitelist.indexOf(cacheName) === -1) {
             return caches.delete(cacheName);
           }
